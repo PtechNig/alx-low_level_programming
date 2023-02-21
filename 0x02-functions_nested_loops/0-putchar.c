20ac861299e81279d785c62ca94051fc32b2f011
PtechNig <oyelamioluwaseunp@gmail.com>
2023-02-21 05:51:51 -0800

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,12 +9,14 @@
 int main(void)
 {
 	
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count;
+	putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+        _putchar('r');
+        _putchar('\n');
 
-	for (count = 0; count < 8; count++;)
-	{
-		_putchar(str[count]);
-	}
-	_putchar("\n");
+	return (0);
 }
